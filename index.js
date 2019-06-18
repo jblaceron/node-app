e0ebaed4ae45a431b86046a232bc7f498fe54b64
Juan Aceron <oneaceron@gmail.com>
2019-06-18 15:43:51 +0800

    changes

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ var app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('hello juan!');
+ res.send('testing ci!');
+});
+
+app.get('/hello', function (req, res) {
+    res.send('hello world!');
 });
 
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
  
+
+
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var app = require('../index.js');
 
 describe('GET /', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/').expect('hello juan!', done);
+        // request(app).get('/').expect('hello juan!', done);
+        request(app).get('/hello').expect('hello world!', done)
     });
 });
